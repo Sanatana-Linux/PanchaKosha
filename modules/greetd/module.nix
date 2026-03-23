8734d7633bd61bbd90dafb97bba5a820d47569ab
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2026-03-22 23:41:24 -0400

    2026-03-22 23:41:24

--- modules/greetd/module.nix
+++ modules/greetd/module.nix
@@ -84,7 +84,7 @@ EOF
   # Build greetd settings based on configuration
   greetdSettings = {
   default_session.command = if cfg.quickshellGreeter.enable
-    then "QT_QPA_PLATFORM=wayland-egl ${pkgs.quickshell}/bin/quickshell --path ${cfg.quickshellGreeter.package}/share/quickshell/greeter"
+    then "env QT_QPA_PLATFORM=wayland-egl ${pkgs.quickshell}/bin/quickshell --path ${cfg.quickshellGreeter.package}/share/quickshell/greeter"
       else cfg.sessionCommand;
     default_session.user = if cfg.quickshellGreeter.enable 
       then "greeter" 
